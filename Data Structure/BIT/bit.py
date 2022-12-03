b9a4b3f7b984f9d6e2483b3ad9c61d2935768d31
huaxing-w <hwang68@syr.edu>
2022-12-02 22:04:49 -0800

    Create bit.py

---
+++ Data Structure/BIT/bit.py
@@ -0,0 +1,38 @@
+class BIT:
+    """范围修改"""
+
+    def __init__(self, n: int):
+        self.size = n
+        self._tree1 = defaultdict(int)
+        self._tree2 = defaultdict(int)
+
+    @staticmethod
+    def _lowbit(index: int) -> int:
+        return index & -index
+
+    def add(self, left: int, right: int, delta: int) -> None:
+        """闭区间[left, right]加delta"""
+        self._add(left, delta)
+        self._add(right + 1, -delta)
+
+    def query(self, left: int, right: int) -> int:
+        """闭区间[left, right]的和"""
+        return self._query(right) - self._query(left - 1)
+
+    def _add(self, index: int, delta: int) -> None:
+        rawIndex = index
+        while index <= self.size:
+            self._tree1[index] += delta
+            self._tree2[index] += (rawIndex - 1) * delta
+            index += self._lowbit(index)
+
+    def _query(self, index: int) -> int:
+        if index > self.size:
+            index = self.size
+
+        rawIndex = index
+        res = 0
+        while index > 0:
+            res += rawIndex * self._tree1[index] - self._tree2[index]
+            index -= self._lowbit(index)
+        return res
