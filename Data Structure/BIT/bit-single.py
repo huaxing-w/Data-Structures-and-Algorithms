6799a0f960768ddd2dd17099fe0e0652eef9f04f
huaxing-w <hwang68@syr.edu>
2023-01-10 19:32:31 -0800

    bit

---
+++ Data Structure/BIT/bit-single.py
@@ -0,0 +1,29 @@
+class BIT:
+    """单点修改"""
+
+    def __init__(self, n: int):
+        self.size = n
+        self.tree = defaultdict(int)
+
+    @staticmethod
+    def _lowbit(index: int) -> int:
+        return index & -index
+
+    def add(self, index: int, delta: int) -> None:
+        if index <= 0:
+            raise ValueError('index 必须是正整数')
+        while index <= self.size:
+            self.tree[index] += delta
+            index += self._lowbit(index)
+
+    def query(self, index: int) -> int:
+        if index > self.size:
+            index = self.size
+        res = 0
+        while index > 0:
+            res += self.tree[index]
+            index -= self._lowbit(index)
+        return res
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.query(right) - self.query(left - 1)
